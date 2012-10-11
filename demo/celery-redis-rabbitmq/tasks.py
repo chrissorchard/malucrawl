824e9b4bca4d4eec9fc0d91c6331b73981719f4f
Thomas Grainger <tagrain@gmail.com>
2012-10-11 20:40:41 +0100

    added demo celery project, setup with RabbitMQ as broker and redis as result backend.

---
+++ demo/celery-redis-rabbitmq/tasks.py
@@ -0,0 +1,12 @@
+from celery import Celery, group
+
+celery = Celery('tasks', backend='redis://:Km7icdOpKvb6JIzN40iG@kanga-cso1g09c', broker='amqp://guest@kanga-cso1g09c//')
+
+@celery.task
+def add(x, y):
+    return x + y
+
+@celery.task
+def addlist(list):
+    return group(add.subtask((i,i)) for i in list).apply_async()
+
