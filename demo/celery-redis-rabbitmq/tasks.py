7711921419bf2fe4fb14dbdd982c092ccaafb494
Thomas Grainger <tagrain@gmail.com>
2012-10-13 01:05:22 +0100

    Added password authentication to RabbitMQ

--- demo/celery-redis-rabbitmq/tasks.py
+++ demo/celery-redis-rabbitmq/tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery, group
 
-celery = Celery('tasks', backend='redis://:Km7icdOpKvb6JIzN40iG@kanga-cso1g09c', broker='amqp://guest@kanga-cso1g09c//')
+celery = Celery('tasks', backend='redis://:Km7icdOpKvb6JIzN40iG@kanga-cso1g09c', broker='amqp://guest:mkP5b9mholFmthIixyNx@kanga-cso1g09c//')
 
 @celery.task
 def add(x, y):
