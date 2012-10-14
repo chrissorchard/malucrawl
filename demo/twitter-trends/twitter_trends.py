7fe1a6ef600ac4f94b5a68c6bc2ab02a9a81fb21
Weike Liao <wl15g09@ecs.soton.ac.uk>
2012-10-14 14:56:50 +0100

    Implemented twitter trends keywords retriving. It prints the trending keywords without hash and spaces properly, using the malucrawl twitter account.

---
+++ demo/twitter-trends/twitter_trends.py
@@ -0,0 +1,36 @@
+import tweepy
+import json
+import pprint
+
+consumer_key="8MJXt6b7JO4D9CQuDdzNLg"
+consumer_secret="pJuG88umFd61hVmqgcv4S1A26B9FROwr1w2nPLBjMHk"
+
+access_token="877860914-fwOaxBigey11rhzETtE5fh5djzvzxFmIDWFVifAi"
+access_token_secret="Pv9j7PhpSktS7OFokWiV84T3GeYJeSSrZsJKrj1O4"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#print api.me().name
+
+pprinter = pprint.PrettyPrinter(indent=2)
+respond = api.trends_location('1')
+respondJson = json.dumps(respond)
+trendList = json.loads(respondJson)[0]['trends']
+for dct in trendList:
+	name = dct['name'].encode('gb18030','ignore')
+	if(name.startswith('#')):
+		name = name.replace('#','',1)
+		tempList = list(name)
+		for i, c in enumerate(tempList):
+			if(i == 0): continue
+			if c.isupper():
+				if(tempList[i+1].isupper() and tempList[i-1].isupper()):
+					continue
+				else:
+					tempList[i] = ' '+tempList[i]
+		name = ''.join(tempList)
+	print name
+	
