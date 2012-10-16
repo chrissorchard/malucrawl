ce5a0501e947fb0ba5d74dac3d2549a975904843
Thomas Grainger <tagrain@gmail.com>
2012-10-16 13:10:44 +0100

    finished creating all the task definitions: for twitter trend, dogpile and malware_scan

--- trend_crawl.py
+++ trend_crawl.py
@@ -5,6 +5,11 @@ import lxml.html
 from urlparse import urlparse, parse_qs, urlunparse
 from urllib import urlencode
 
+
+from celery import Celery, group
+
+celery = Celery('tasks', backend='redis://:Km7icdOpKvb6JIzN40iG@kanga-cso1g09c', broker='amqp://guest:mkP5b9mholFmthIixyNx@kanga-cso1g09c//')
+
 consumer_key = "8MJXt6b7JO4D9CQuDdzNLg"
 consumer_secret = "pJuG88umFd61hVmqgcv4S1A26B9FROwr1w2nPLBjMHk"
 
@@ -17,25 +22,45 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-def camelCaseSentenceToCase(string):
+def to_unicode(obj, encoding='utf-8'):
+    # http://farmdev.com/talks/unicode/
+    if isinstance(obj,basestring):
+        if not isinstance(obj, unicode):
+            obj = unicode(obj,encoding)
+    return obj
+
+def camelCaseToSentenceCase(string):
     # http://stackoverflow.com/a/9283563
     return re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', string)
 
-
+@celery.task
 def twitter_trend():
+    
     trend_list = api.trends_location('1')[0]['trends']
-
-    for name in (dct["name"] for dct in trend_list):
+    
+    def hash_tag_handle(name):
         if name.startswith('#'):
-            name = camelCaseSentenceToCase(name[1:])
-        print name
-	
+            name = camelCaseToSentenceCase(name[1:])
+        return name
+
+    return [hash_tag_handle(to_unicode(item["name"])) for item in trend_list]
+
+@celery.task	
 def search(keyword):
+    base_url = urlunparse(("http","www.dogpile.co.uk","/search/web",'',urlencode({"q":keyword}),''))
     return map(
         lambda link: parse_qs(urlparse(link.get('href')).query)["du"][0],
         lxml.html.fromstring(
-            requests.get(urlunparse(
-                ("http","www.dogpile.co.uk","/search/web",'',urlencode({"q":keyword}),'')
-            )).text
+            requests.get(base_url).text,
+            base_url = base_url
         ).cssselect(".webResult .resultDisplayUrl")
     )
+
+@celery.task
+def malware_scan(url):
+    e_title = lxml.html.fromstring(
+        requests.get(url).text,
+        base_url = url
+    ).cssselect("title")
+    if e_title:
+        return e_title[0].text
