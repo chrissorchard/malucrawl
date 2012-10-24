26a6eb3aea8172d8675f05cb4f4f4df374d8b010
Thomas Grainger <tagrain@gmail.com>
2012-10-24 20:47:19 +0100

    PEP8 settings.py

--- malucrawl/settings.py
+++ malucrawl/settings.py
@@ -4,7 +4,7 @@ import djcelery
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-COMPRESS_ENABLED=True
+COMPRESS_ENABLED = True
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
