abcf52c5f50b20753fedcab9928e33b7022f1e35
Nafiseh <nv2g09@soton.ac.uk>
2012-11-13 17:16:15 +0000

    Added the add_url which is responsible for filling out the list used in classificaiton

--- demo/classifier.py
+++ demo/classifier.py
@@ -1,5 +1,7 @@
 from pebl.util import levenshtein
 
+
+_mal_list = []
 """
 def levenshtein(s1, s2):
     l1 = len(s1)
@@ -47,8 +49,8 @@ def classify_is_sus_urls(database_dict, urls):
 def classify_is_sus(database, new_url):
     sus_sum = 0
     notsus_sum = 0
-    sind = 0
-    nind = 0
+    sind = 1
+    nind = 1
     for item in database:
         dist = levenshtein(new_url, item["url"])
         if item["malware"]:
@@ -57,9 +59,24 @@ def classify_is_sus(database, new_url):
         else:
             notsus_sum = notsus_sum + dist
             nind = nind + 1
+	avg_sus = sus_sum/sind
+	avg_notsus = notsus_sum/nind
+	total_len = avg_sus + avg_notsus
+	if avg_sus > avg_notsus:
+		return False, 1.0 - float(avg_notsus)/float(total_len)
+	else:
+		return True, 1.0 - float(avg_sus)/float(total_len)
 
-    return sus_sum / sind <= notsus_sum / nind
 
+def add_url(url, is_mal):
+	""" Gets url and if it is a malware and fills the 
+		_mal_list which is used for the classification task
+
+		url -- the url which is classified as malware or not 
+		is_mal -- true or false 
+	"""
+	global _mal_list
+	_mal_list.append ({"url":url, "malware":is_mal})
 
 if __name__ == '__main__':
     import json
