a3a5a47ac577cbf7efa613ba6e8fc15ed499c41f
Thomas Grainger <tagrain@gmail.com>
2013-01-08 22:00:53 +0000

    only one redis server today

--- malucrawl/deploy_settings.py
+++ malucrawl/deploy_settings.py
@@ -7,7 +7,7 @@ BROKER_URL = "amqp://guest:mkP5b9mholFmthIixyNx@malucrawl.ecs.soton.ac.uk//"
 
 MALUCRAWL_REDIS = {
     "master": "redis://:Km7icdOpKvb6JIzN40iG@malucrawl.ecs.soton.ac.uk:6379/0",
-    "slave": "redis://:Km7icdOpKvb6JIzN40iG@localhost:6379/0"
+    "slave": "redis://:Km7icdOpKvb6JIzN40iG@malucrawl.ecs.soton.ac.uk:6379/0"
 }
 
 CELERY_RESULT_BACKEND = MALUCRAWL_REDIS["master"]
